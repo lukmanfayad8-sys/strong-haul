9e61400fb6ec40614650609bd9b442d21b9023e4
Lukman Fayad <lukmanfayad8@gmail.com>
2026-05-21 16:25:42 -0700

    save

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.jsx";
 import BrowseTrucks from "./pages/BrowseTrucks.jsx";
+import Auth from "./pages/Auth.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/browse" element={<BrowseTrucks />} />
-      </Routes>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/browse" element={<BrowseTrucks />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
     </BrowserRouter>
   </StrictMode>
 );
