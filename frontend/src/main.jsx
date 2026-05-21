0ec76f8d67fa2aafb2253c747d85a40895caba29
Lukman Fayad <lukmanfayad8@gmail.com>
2026-05-21 16:26:30 -0700

    save

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="/browse" element={<BrowseTrucks />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
