fcad64e0b59b5dc3866ad50e66ad4592592ba822
Lukman Fayad <lukmanfayad8@gmail.com>
2026-05-22 02:48:11 -0700

    save

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import AuthProvider from "./context/AuthProvider.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
 import BrowseTrucks from "./pages/BrowseTrucks.jsx";
 import Auth from "./pages/Auth.jsx";
