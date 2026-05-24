ee5def058ba31186f4199b51bb8cf37f2deabf7d
Lukman Fayad <lukmanfayad8@gmail.com>
2026-05-24 07:55:45 -0700

    save

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,11 @@ app.openapi = custom_openapi
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_origin],
+  allow_origins=[
+    frontend_origin,
+    "https://strong-haul.vercel.app",
+    "http://localhost:5173",
+ ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
